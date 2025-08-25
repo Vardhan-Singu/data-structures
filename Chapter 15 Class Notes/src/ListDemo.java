5a499ce9036b277826da6cb3bd704ffd6069bad9
Mona List <email@example.com>
2025-08-25 13:17:04 -0500

    Day 1 of LinkedList Demo
    finished with iterator

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,30 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        /* The addLast method can be used to populate a list. */
+        LinkedList<String> staff = new LinkedList<>();
+        staff.addLast("Tony");
+        staff.addLast("Natasha");
+        staff.addLast("Steve");
+        staff.addLast("Tyrone");
+
+        System.out.println("Here is the staff list:");
+        System.out.println(staff);
+
+        // The list is currently: TNST
+        /*
+         * The lsitIterator method creates a new list iterator.
+         * that is positioned at the head of the list.
+         * The | is used ti reoresent the iterator's position.
+         */
+        ListIterator<String> iterator = staff.listIterator(); //|TNST
+
+        /* The next method advances the iterator OVER the next element in the list.
+         */
+        iterator.next(); // T|NST
+        // The next method also returns the element that the iterator passes over.
+        String avenger = iterator.next();
+        System.out.println(avenger + " is an Avenger.");
         
     }
 }
