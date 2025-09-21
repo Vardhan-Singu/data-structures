f22b9d85b95875d7e8c099acb2f1d1d1a0434eaa
Vardhan-Singu <gcsingu1@stu.naperville203.org>
2025-09-21 17:06:55 -0500

    I'm really confused about this one can we look at it in class, I think it's right for now but I can't wrap my head around why

--- Chapter 15 Activities/FloodFill/src/Grid.java
+++ Chapter 15 Activities/FloodFill/src/Grid.java
@@ -1,15 +1,42 @@
+import java.util.Stack;
+
 public class Grid
 {
     private static final int SIZE = 10;
     int[][] pixels = new int[SIZE][SIZE];
-    . . .
+    
 
     /**
      * Flood fill, starting with the given row and column.
     */
     public void floodfill(int row, int column)
     {
-        . . .
+        Stack<Integer> stack = new Stack<>(); // Stack to hold the rows and columns as integers
+        int counter = 1; // Counter to fill in the pixels
+
+        stack.push(row); // First row
+        stack.push(column); // First column
+
+        while (!stack.isEmpty()) {
+            int col = stack.pop(); // pops the comumn first
+            int r = stack.pop(); // pops the row second
+
+            // Check if the rows and columbs are filled
+            if (r >=0 && r < SIZE && col >= 0 && col < SIZE && pixels[r][col] == 0) {
+                pixels[r][col] = counter; // Fill the pixel
+                counter++; // Increment the counter
+                // Okay so the rows will go top to bottom and the columns will go left to right
+                stack.push(r);
+                stack.push(col+1);
+                stack.push(r);
+                stack.push(col-1);
+                stack.push(r+1);
+                stack.push(col );
+                stack.push(r-1);
+                stack.push(col);
+                // This is really confusing I wish I could visualize it better
+            }
+        }
     }
 
     @Override
